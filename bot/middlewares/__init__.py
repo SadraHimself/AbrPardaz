dc0b9457012c0668a6132d7de07156e2862c23f1
100Ra <154072421+SadraHimself@users.noreply.github.com>
2026-06-26 22:24:59 +0330

    Add RateLimitMiddleware to __all__ exports

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,4 +1,5 @@
 from .auth import AuthMiddleware
 from .db import DbSessionMiddleware
+from .rate_limit import RateLimitMiddleware
 
-__all__ = ["DbSessionMiddleware", "AuthMiddleware"]
+__all__ = ["DbSessionMiddleware", "AuthMiddleware", "RateLimitMiddleware"]
